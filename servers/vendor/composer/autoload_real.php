58e4804082ef1488d8f9d977a8c2ed8d4f313727
ZhuYunlong <920200256@qq.com>
2019-02-19 10:01:53 +0800

    去掉config

--- servers/vendor/composer/autoload_real.php
+++ servers/vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit34a41e2841af1a67f3ddef099fc7b348
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit34a41e2841af1a67f3ddef099fc7b348', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
